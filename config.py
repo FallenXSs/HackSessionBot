4cf192903160493f74dcba68d8a02a783bf34d13
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-12-02 17:30:36 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     API_ID = "25719105"
     API_HASH = "5ac96667719700bf3f9cb0d5fb30b748"
     TOKEN = "6409244695:AAG8836inA1LBbp_ui9_0O9epV33XPGTIK4"  
-    START_PIC = "" 
+    START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg" 
     CHAT = "@FallenTr"    
