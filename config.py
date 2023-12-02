2d1eefbc082171d836035335e6b9e50426d29d76
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-12-02 17:30:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
+    API_ID = "25719105"
+    API_HASH = "5ac96667719700bf3f9cb0d5fb30b748"
+    TOKEN = "6409244695:AAG8836inA1LBbp_ui9_0O9epV33XPGTIK4"  
     START_PIC = "" 
-    CHAT = ""    
+    CHAT = "@FallenTr"    
